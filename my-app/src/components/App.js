9e8a142461976d51e9d7c3b415fde19c302ade27
+ <+>
2022-09-08 12:09:37 +0300

    commit

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,4 +1,4 @@
-
+import {Switch, Route} from 'react-router-dom'
 import React from 'react'
 import NavBar from './NavBar'
 import LoginPage from './LoginPage'
@@ -11,11 +11,11 @@ function App() {
     
     <div className="App">
       <NavBar />
-      <Switch>
+      
         <Route path="/level"> <LevelPage/> </Route>
         <Route path="/score"> <ScorePage/> </Route>
         <Route path="/"> <LoginPage/> </Route>
-      </Switch>
+      
     </div>
 
   );
